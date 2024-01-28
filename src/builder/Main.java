077b7b5cfd736a5b48494fd62c6eb32824ed75b6
rajatkale7 <rajatkale7@gmail.com>
2024-01-28 18:28:13 +0530

    SundayCommit

--- src/builder/Main.java
+++ src/builder/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Student s= Student.builder()
                 .id(1)
                 .name("Rajat")
-                .age(24)
+                .age(19)
                 .phoneNumber("9545823345")
                 .universityName("Solapur university")
                 .psp(90)
@@ -14,3 +14,19 @@ public class Main {
         System.out.println(s);
     }
 }
+
+/*
+FLOW OF EXECUTION:
+
+~Student.builder() gives us the Builder class.
+~Builder class.id() is initiating id attribute of Builder class also returning Builder class
+~now, Builder.build()--> build() method is first validating the data and if no exception thrown,
+it will return a Student class with all the values passed inside Student() constructor from
+Builder class.
+~So, final return is Student class. So, we stored it inside Student.
+
+
+Let's see whether we achieved our goal or not:
+~ we are supposed to design a system which will store student data
+and finally it stored.
+ */
