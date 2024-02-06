804d9ed9ae7c0c5fa39122f2f7a21900baab6d2e
rajatkale7 <rajatkale7@gmail.com>
2024-02-06 23:21:48 +0530

    done

--- src/decorator/Main.java
+++ src/decorator/Main.java
@@ -3,12 +3,12 @@ package decorator;
 public class Main {
         public static void main(String[] args) {
             IceCream ic = new ChocoChip(
-                    new ChocoScoop(
-                            new OrangeCone(
-                                    new ChocoSyrup(
-                                            new ChocoCone()))));
+                               new ChocoScoop(
+                                       new OrangeCone(
+                                                new ChocoSyrup(
+                                                        new ChocoCone()))));
 
             System.out.println("Price is "+ ic.getCost());
-            System.out.println(ic.getDescription());
+            System.out.println("My Icecream is--> "+ic.getDescription());
         }
 }
