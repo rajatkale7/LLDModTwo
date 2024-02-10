9875ad0143fe3aca8e1662a1594e7adac9049e00
rajatkale7 <rajatkale7@gmail.com>
2024-02-10 23:12:41 +0530

    done

--- src/stretegyDesignPattern/Maps.java
+++ src/stretegyDesignPattern/Maps.java
@@ -4,6 +4,5 @@ public class Maps {
     public void givePath(String source, String destination, Mode mode){
         PathCalculator pc= PathCalculatorFactory.getPathCalculator(mode);  //object will get created
         pc.findPath(source,destination);    //object.method
-
     }
 }
