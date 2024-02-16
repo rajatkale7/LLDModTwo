5b7fc3eb9a950e121bc8b5ab752e5e40b931ee63
rajatkale7 <rajatkale7@gmail.com>
2024-02-17 00:23:02 +0530

    done

--- src/stretegy/Maps.java
+++ src/stretegy/Maps.java
@@ -2,7 +2,8 @@ package stretegy;
 
 public class Maps {
     public void givePath(String source, String destination, Mode mode){
-        PathCalculator pc= PathCalculatorFactory.getPathCalculator(mode);  //object will get created
+        PathCalculator pc= PathCalculatorFactory.getPathCalculator(mode);  //Method is static-->So,Class.method()
+        //PathCalculatorFactory class is a house of objects.
         pc.findPath(source,destination);    //object.method
     }
 }
