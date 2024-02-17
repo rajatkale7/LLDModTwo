fdb1354d618fd7d46d4f5efe4e5abf16f9ea413c
rajatkale7 <rajatkale7@gmail.com>
2024-02-18 01:54:57 +0530

    done

--- src/stretegy/rideSharingApp/Factory.java
+++ src/stretegy/rideSharingApp/Factory.java
@@ -1,11 +1,11 @@
 package stretegy.rideSharingApp;
 
 public class Factory {
-    public static PricingStrategy getStrategy(PricingType pricingType){
+    public static PricingStrategy getStrategy(PricingType pricingType) {
         return switch (pricingType) {
             case DISTANCE_BASED -> new DistanceBased();
             case TIME_BASED -> new TimeBased();
-            case SURGE ->new  SurgeBased();
+            case SURGE -> new SurgeBased();
         };
     }
 }
