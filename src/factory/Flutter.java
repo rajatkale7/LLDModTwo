54d1a4c1f6603cb0fe0e3f91108953dde0254c9e
rajatkale7 <rajatkale7@gmail.com>
2024-02-20 03:31:17 +0530

    done

---
+++ src/factory/Flutter.java
@@ -0,0 +1,12 @@
+package factory;
+
+public class Flutter {
+    private SupportedPlatforms supportedPlatforms;
+    public void setTheme(){
+        System.out.println("Theme is being set");
+    }
+
+    public void setRefreshRate(){
+        System.out.println("Refresh rate is being set");
+    }
+}
